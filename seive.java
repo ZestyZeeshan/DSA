bf01c8fe50cff61129384080d629d469d6b987a0
ZestyZeeshan <141507225+ZestyZeeshan@users.noreply.github.com>
2023-10-04 23:15:41 +0530

    Add files via upload

---
+++ seive.java
@@ -0,0 +1,21 @@
+public class seive {
+    public static void main(String[] args){
+        int n=40;
+        boolean [] primes = new boolean [n+1];
+       // System.out.print(prime[0]);
+        seivee(n,primes);
+    }static void seivee(int n ,boolean[] prime){
+        for(int i=2;i*i<=n;i++){
+            if(!prime[i]){
+                for(int j=i*2;j<=n;j+=i){
+                    prime[j]=true;
+                }
+            }
+        }
+        for(int i=2;i<=n;i++){
+            if(!prime[i]){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
